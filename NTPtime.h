a7ae0a22d1e66ef3de79c99646cabe7ac086ef16
Oleksandr <amin7@ukr.net>
2017-02-11 09:41:04 +0200

    2

---
+++ NTPtime.h
@@ -0,0 +1,33 @@
+/*
+ * NTPtime.h
+ *
+ *  Created on: 4 ???. 2017 ?.
+ *      Author: User
+ */
+
+#ifndef CLOCK_NTPTIME_H_
+#define CLOCK_NTPTIME_H_
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+class NTPtime {
+private:
+	IPAddress timeServerIP; // time.nist.gov NTP server address
+	const char* ntpServerName = "time.nist.gov";
+
+	const static int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+
+	byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+	const static unsigned int localPort = 2390;      // local port to listen for UDP packets
+	WiFiUDP udp;
+	unsigned long sendNTPpacket(IPAddress& address);
+public:
+	NTPtime();
+	void init();
+	unsigned long getTime();
+	virtual ~NTPtime();
+};
+
+#endif /* CLOCK_NTPTIME_H_ */
---
+++ clock.ino
@@ -0,0 +1,95 @@
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Max72xxPanel.h>
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "NTPtime.h"
+
+char ssid[] = "Guest1";  //  your network SSID (name)
+char pass[] = "MH-6346PQMS";       // your network password
+
+
+int pinCS = 2; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
+int numberOfHorizontalDisplays = 4;
+int numberOfVerticalDisplays = 1;
+
+Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+
+String tape = "Arduino";
+int wait = 20; // In milliseconds
+
+int spacer = 1;
+int width = 5 + spacer; // The font width is 5 pixels
+
+NTPtime ntpTime;
+
+void setup() {
+
+  matrix.setIntensity(1); // Use a value between 0 and 15 for brightness
+  matrix.setRotation(0,1);
+  matrix.setRotation(1,1);
+  matrix.setRotation(2,1);
+  matrix.setRotation(3,1);
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  ntpTime.init();
+  matrix.setTextColor(HIGH, LOW);
+  matrix.setTextSize(1);
+  matrix.print("test");
+}
+
+void loop() {
+	unsigned long time =ntpTime.getTime();
+	if(time){
+		matrix.fillScreen(LOW);
+
+		matrix.setCursor(0,0);
+		unsigned char sec=time%60;
+		Serial.println(sec);
+		matrix.print(sec);
+		matrix.write();
+		delay(100);
+	}
+#if 0
+  for ( int i = 0 ; i < width * tape.length() + matrix.width() - 1 - spacer; i++ ) {
+
+    matrix.fillScreen(LOW);
+
+    int letter = i / width;
+    int x = (matrix.width() - 1) - i % width;
+    int y = (matrix.height() - 8) / 2; // center the text vertically
+
+    while ( x + width - spacer >= 0 && letter >= 0 ) {
+      if ( letter < tape.length() ) {
+        matrix.drawChar(x, y, tape[letter], HIGH, LOW, 1);
+      }
+
+      letter--;
+      x -= width;
+    }
+
+    matrix.write(); // Send bitmap to display
+
+    delay(wait);
+  }
+#endif
+}
