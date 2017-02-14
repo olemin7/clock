9d89d600a50d923e2b5343343d17e90df12c63dc
Oleksandr Minenko <ominenko@intropro.com>
2017-02-14 14:38:03 +0200

    +asink ntp time read

--- NTPtime.cpp
+++ NTPtime.cpp
@@ -15,48 +15,47 @@ NTPtime::NTPtime() {
 
 void NTPtime::init(){
 	 udp.begin(localPort);
-	 WiFi.hostByName(ntpServerName, timeServerIP);
+         WiFi.hostByName(ntpServerName, timeServerIP);
 }
 
-int32 NTPtime::parceAsEpoch(){
-	refreshed=millis();
-	udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+int32 NTPtime::parceAsEpoch() {
+  refreshed = millis();
+  udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
 
-	//the timestamp starts at byte 40 of the received packet and is four bytes,
-	// or two words, long. First, esxtract the two words:
+  // the timestamp starts at byte 40 of the received packet and is four bytes,
+  // or two words, long. First, esxtract the two words:
 
-	unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-	unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-	// combine the four bytes (two words) into a long integer
-	// this is NTP time (seconds since Jan 1 1900):
-	unsigned long secsSince1900 = highWord << 16 | lowWord;
-	const unsigned long seventyYears = 2208988800UL;
-	// subtract seventy years:
-	return secsSince1900 - seventyYears;
+  unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+  unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+  // combine the four bytes (two words) into a long integer
+  // this is NTP time (seconds since Jan 1 1900):
+  unsigned long secsSince1900 = highWord << 16 | lowWord;
+  const unsigned long seventyYears = 2208988800UL;
+  // subtract seventy years:
+  return secsSince1900 - seventyYears;
 }
 
-int  NTPtime::getTime(int32 &epoch){
-	sendNTPpacket();
-	  // wait to see if a reply is available
-	  int i=100;
-	  do{
-		  if(0==i){
-			  return 1;
-		  }
-		  i--;
-		  delay(10);
+int NTPtime::getTime(int32 &epoch) {
+  sendNTPpacket();
+  // wait to see if a reply is available
+  int i = 100;
+  do {
+    if (0 == i) {
+      return 1;
+    }
+    i--;
+    delay(10);
 
-    }while(0==udp.parsePacket());
-	// We've received a packet, read the data from it
-	epoch= parceAsEpoch();
-	return 0;
+  } while (0 == udp.parsePacket());
+  // We've received a packet, read the data from it
+  epoch = parceAsEpoch();
+  return 0;
 }
 
-int NTPtime::sendNTPpacket()
-{
+int NTPtime::sendNTPpacket() {
   // all NTP fields have been given values, now
   // you can send a packet requesting a timestamp:
-	  // set all bytes in the buffer to 0
+  // set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
   // Initialize values needed to form NTP request
   // (see URL above for details on the packets)
@@ -69,43 +68,43 @@ int NTPtime::sendNTPpacket()
   packetBuffer[13]  = 0x4E;
   packetBuffer[14]  = 49;
   packetBuffer[15]  = 52;
-  udp.beginPacket(timeServerIP, 123); //NTP requests are to port 123
+  udp.beginPacket(timeServerIP, 123); // NTP requests are to port 123
   udp.write(packetBuffer, NTP_PACKET_SIZE);
   return udp.endPacket();
 }
 
-bool NTPtime::getTimeAsink(int32 &epoch){
-	const uint32 curms=millis();
+bool NTPtime::getTimeAsink(int32 &epoch) {
+  const uint32 curms = millis();
 #ifdef DUBUG_CLOCK_NTPTIME
-	Serial.println();
-	Serial.print(curms);
-	Serial.print(" ");
-	Serial.print(curms-refreshed);
+  Serial.println();
+  Serial.print(curms);
+  Serial.print(" ");
+  Serial.print(curms - refreshed);
 
 #endif
-	if((refreshed+refreshPeriodDef)>curms){
-		return false;//time out is not passed
-	}
+  if ((refreshed + refreshPeriodDef) > curms) {
+    return false; // time out is not passed
+  }
 #ifdef DUBUG_CLOCK_NTPTIME
-	Serial.print(" ");
-	Serial.print(curms-sendOn);
+  Serial.print(" ");
+  Serial.print(curms - sendOn);
 #endif
-	if((sendOn+sendDiscardPeriod)<curms){//send or resend request
-		sendNTPpacket();
-		sendOn=millis();
-		return false;
-	}
-	//wait reply
+  if ((sendOn + sendDiscardPeriod) < curms) { // send or resend request
+    sendNTPpacket();
+    sendOn = millis();
+    return false;
+  }
+// wait reply
 #ifdef DUBUG_CLOCK_NTPTIME
-	Serial.print("wait reply");
+  Serial.print("wait reply");
 #endif
-	if (0==udp.parsePacket()) {
-			return false;
-	}
-	sendOn=0;
-	refreshed=millis();
-	epoch= parceAsEpoch();
-	return true;
+  if (0 == udp.parsePacket()) {
+    return false;
+  }
+  sendOn = 0;
+  refreshed = millis();
+  epoch = parceAsEpoch();
+  return true;
 }
 
 NTPtime::~NTPtime() {
--- NTPtime.h
+++ NTPtime.h
@@ -14,27 +14,30 @@
 
 class NTPtime {
 private:
-	const static uint32 refreshPeriodDef=10*1000;//ms
-	const static uint32 sendDiscardPeriod=1000;//ms
-	uint32 refreshed=0;//ms
-	uint32 sendOn=0;//ms
-	IPAddress timeServerIP; // time.nist.gov NTP server address
-	const char* ntpServerName = "time.nist.gov";
+  const static uint32 refreshPeriodDef = 10 * 1000; // ms
+  const static uint32 sendDiscardPeriod = 1000;     // ms
+  uint32 refreshed = 0;                             // ms
+  uint32 sendOn = 0;                                // ms
+  IPAddress timeServerIP; // time.nist.gov NTP server address
+  const char *ntpServerName = "time.nist.gov";
 
-	const static int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+  const static int NTP_PACKET_SIZE =
+      48; // NTP time stamp is in the first 48 bytes of the message
 
-	byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming packets
+  byte packetBuffer[NTP_PACKET_SIZE]; // buffer to hold incoming packets
 
-	const static unsigned int localPort = 2390;      // local port to listen for UDP packets
-	WiFiUDP udp;
-	int32 parceAsEpoch();
-	int sendNTPpacket();
+  const static unsigned int localPort =
+      2390; // local port to listen for UDP packets
+  WiFiUDP udp;
+  int32 parceAsEpoch();
+  int sendNTPpacket();
+
 public:
 	NTPtime();
 	void init();
 	int getTime(int32 &epoch);
-	bool getTimeAsink(int32 &epoch);
-	virtual ~NTPtime();
+        bool getTimeAsink(int32 &epoch);
+        virtual ~NTPtime();
 };
 
 #endif /* CLOCK_NTPTIME_H_ */
