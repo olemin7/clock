5fccb8c7defc23ec164c12e6ae8924b5c0e65a22
Oleksandr Minenko <oleksandr.minenko@ringteam.com>
2023-10-24 21:54:48 +0300

    update

--- src/clock.cpp
+++ src/clock.cpp
@@ -1,48 +1,82 @@
-#include "clock.h"
-using namespace std;
+// clang-format off
+#include <NTPClient.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <LittleFS.h>
 
-constexpr auto SERIAL_BAUND = 115200;
+#include <Wire.h> // must be included here so that Arduino library object file references work
+#include <pgmspace.h>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <ctime>
+#include <Max72xxPanel.h> // https://github.com/markruys/arduino-Max72xxPanel.git
+#include <FreeMono9pt7b.h>
+
+#include "DHTesp.h"
+
+#include <CMQTT.h>
+
+// clang-format on
+#include <misk.h>
+#include <wifiHandle.h>
+#include <logs.h>
+#include <CConfig.h>
+#include "CLDRSignal.h"
+
+#include <eventloop.h>
+
+#include <WiFiUdp.h>
+#include <Timezone.h>
+
+#define DEBUG
+
+using namespace std;
+using namespace std::chrono_literals;
+
+constexpr auto SERIAL_BAUND    = 115200;
 constexpr auto SERVER_PORT_WEB = 80;
 
-constexpr auto AP_MODE_TIMEOUT = 30 * 1000;  // switch to ap if no wifi
-constexpr auto AUTO_REBOOT_AFTER_AP_MODE =
-    5 * 60 * 1000;  // switch to ap if no wifi
+constexpr auto AP_MODE_TIMEOUT           = 30s; // switch to ap if no wifi
+constexpr auto AUTO_REBOOT_AFTER_AP_MODE = 5min; // switch to ap if no wifi
 
-constexpr auto pinCS = D6;
+constexpr auto pinCS                      = D6;
 constexpr auto numberOfHorizontalDisplays = 4;
-constexpr auto numberOfVerticalDisplays = 1;
+constexpr auto numberOfVerticalDisplays   = 1;
 
 constexpr auto DHTPin = D4;
 
-const char *update_path = "/firmware";
-constexpr auto DEF_AP_PWD = "12345678";
-
-Timezone myTZ((TimeChangeRule ) { "DST", Last, Sun, Mar, 3, +3 * 60 },
-        (TimeChangeRule ) { "STD", Last, Sun, Oct, 4, +2 * 60 });
+const char*    update_path = "/firmware";
+constexpr auto DEF_AP_PWD  = "12345678";
 
 auto matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
 
-NTPtime ntpTime;
-void mqtt_send();
-
-CLDRSignal LDRSignal;
-const char *pDeviceName = nullptr;
-
-class CTimeKeeper: public SignalLoop<time_t> {
-    bool getValue(time_t &val) {
-        val = myTZ.toLocal(now());
-        return true;
-    }
-} timeKeeper;
+CLDRSignal  LDRSignal;
+const char* pDeviceName = nullptr;
 
 DHTesp dht;
 
-ESP8266WebServer serverWeb(SERVER_PORT_WEB);
-CMQTT mqtt;
+ESP8266WebServer        serverWeb(SERVER_PORT_WEB);
+CMQTT                   mqtt;
 ESP8266HTTPUpdateServer otaUpdater;
-CWifiStateSignal wifiStateSignal;
-cevent_loop event_loop;
-auto config = CConfig<512>();
+WiFiUDP                 ntpUDP;
+NTPClient               ntp_client(ntpUDP);
+auto                    config = CConfig<512>();
+Timezone                myTZ((TimeChangeRule){ "DST", Last, Sun, Mar, 3, +3 * 60 },
+                   (TimeChangeRule){ "STD", Last, Sun, Oct, 4, +2 * 60 });
+
+void display_print(std::string str) {
+    matrix.fillScreen(LOW);
+    matrix.setCursor(0, 7);
+    matrix.print(str.c_str());
+    matrix.write();
+    DBG_OUT << "display[" << str << "]" << endl;
+}
 
 void setup_matrix() {
     matrix.setIntensity(0); // Use a value between 0 and 15 for brightness
@@ -55,15 +89,12 @@ void setup_matrix() {
     }
     matrix.setFont(&FreeMono9pt7b);
     matrix.setTextWrap(false);
-    matrix.fillScreen(LOW);
     matrix.setTextColor(HIGH, LOW);
     matrix.setTextSize(1);
-    matrix.setCursor(0, 7);
-    matrix.print("init");
-    matrix.write();
+    display_print("init");
 }
 
-te_ret get_about(ostream &out) {
+te_ret get_about(ostream& out) {
     out << "{";
     out << "\"firmware\":\"clock " << __DATE__ << " " << __TIME__ << "\"";
     out << ",\"deviceName\":\"" << pDeviceName << "\"";
@@ -72,23 +103,17 @@ te_ret get_about(ostream &out) {
     return er_ok;
 }
 
-te_ret get_status(ostream &out) {
-  const auto local = timeKeeper.getLastValue();
-  out << "{\"timeStatus\":" << timeStatus();
-  out << ",\"time\":\"";
-  toTime(out, local);
-  out << " ";
-  toDate(out, local);
-  out << "\"";
-  out << ",\"temperature\":";
-  toJson(out, dht.getTemperature());
-  out << ",\"humidity\":";
-  toJson(out, dht.getHumidity());
-  out << ",\"ldr_raw\":" << LDRSignal.getLDR();
-  out << ",\"ldr\":" << static_cast<unsigned>(LDRSignal.getLastValue());
-  out << ",\"mqtt\":" << mqtt.isConnected();
-  out << "}";
-  return er_ok;
+te_ret get_status(ostream& out) {
+    out << "{";
+    out << "\"temperature\":";
+    toJson(out, dht.getTemperature());
+    out << ",\"humidity\":";
+    toJson(out, dht.getHumidity());
+    out << ",\"ldr_raw\":" << LDRSignal.getLDR();
+    out << ",\"ldr\":" << static_cast<unsigned>(LDRSignal.getLastValue());
+    out << ",\"mqtt\":" << mqtt.isConnected();
+    out << "}";
+    return er_ok;
 }
 
 void setup_WebPages() {
@@ -101,13 +126,9 @@ void setup_WebPages() {
         ESP.restart();
     });
 
-    serverWeb.on("/about", [] {
-        wifiHandle_send_content_json(serverWeb, get_about);
-    });
+    serverWeb.on("/about", [] { wifiHandle_send_content_json(serverWeb, get_about); });
 
-    serverWeb.on("/status", [] {
-        wifiHandle_send_content_json(serverWeb, get_status);
-    });
+    serverWeb.on("/status", [] { wifiHandle_send_content_json(serverWeb, get_status); });
 
     serverWeb.on("/filesave", []() {
         DBG_FUNK();
@@ -130,22 +151,12 @@ void setup_WebPages() {
         webRetResult(serverWeb, er_ok);
     });
 
-    serverWeb.on("/scanwifi", HTTP_ANY,
-            [&]() {
-                wifiHandle_sendlist(serverWeb);
-            });
-    serverWeb.on("/connectwifi", HTTP_ANY,
-                 [&]() { wifiHandle_connect(pDeviceName, serverWeb, true); });
+    serverWeb.on("/scanwifi", HTTP_ANY, [&]() { wifiHandle_sendlist(serverWeb); });
+    serverWeb.on("/connectwifi", HTTP_ANY, [&]() { wifiHandle_connect(pDeviceName, serverWeb, true); });
 
-    serverWeb.on("/getlogs", HTTP_ANY,
-            [&]() {
-                serverWeb.send(200, "text/plain", log_buffer.c_str());
-                log_buffer = "";
-            });
-
-    serverWeb.on("/set_time", [&]() {
-        DBG_FUNK();
-        //todo
+    serverWeb.on("/getlogs", HTTP_ANY, [&]() {
+        serverWeb.send(200, "text/plain", log_buffer.c_str());
+        log_buffer = "";
     });
 
     serverWeb.serveStatic("/", LittleFS, "/www/");
@@ -160,111 +171,118 @@ void setup_WebPages() {
 
 void setup_WIFIConnect() {
     DBG_FUNK();
-    static int16_t to_ap_mode_thread = 0;
     WiFi.hostname(pDeviceName);
     WiFi.begin();
 
-    wifiStateSignal.onChange([](const wl_status_t &status) {
-      if (WL_CONNECTED == status) {
-        // skip AP mode
-        event_loop.remove(to_ap_mode_thread);
-      }
-      wifi_status(cout);
+    auto to_ap_mode_thread = event_loop::set_timeout(
+        []() {
+            display_print("AP");
+            setup_wifi("", DEF_AP_PWD, pDeviceName, WIFI_AP, false);
+
+            event_loop::set_timeout(
+                []() {
+                    DBG_OUT << "Rebooting" << std::endl;
+                    ESP.restart();
+                },
+                AUTO_REBOOT_AFTER_AP_MODE);
+        },
+        AP_MODE_TIMEOUT);
+    static WiFiEventHandler onStationModeConnected = WiFi.onStationModeConnected([](auto event) {
+        DBG_OUT << "WiFi conected, ssid =" << event.ssid << ", channel=" << static_cast<unsigned>(event.channel)
+                << endl;
     });
 
-    to_ap_mode_thread = event_loop.set_timeout( []() {
-      matrix.fillScreen(LOW);
-      matrix.setCursor(0, 7);
-      matrix.print("AP");
-      matrix.write();
-      WiFi.persistent(false);
-      setup_wifi("", DEF_AP_PWD, pDeviceName, WIFI_AP, false);
+    static WiFiEventHandler onStationModeGotIP = WiFi.onStationModeGotIP([to_ap_mode_thread](auto event) {
+        to_ap_mode_thread->cancel();
+        char buffMin[6];
+        sprintf_P(buffMin, ".%03u", event.ip[3]);
+        display_print(buffMin);
+        DBG_OUT << "WiFi IP=" << event.ip << ", mask=" << event.mask << ", gw=" << event.gw << endl;
+    });
 
-      event_loop.set_timeout( []() {
-        DBG_OUT << "Rebooting" << std::endl;
-        ESP.restart();
-      },AUTO_REBOOT_AFTER_AP_MODE);
-    },AP_MODE_TIMEOUT);
+    static WiFiEventHandler onStationModeDisconnected = WiFi.onStationModeDisconnected([](const auto event) {
+        DBG_OUT << "WiFi Disconected, reason =" << static_cast<unsigned>(event.reason) << endl;
+    });
 
     if (WIFI_STA == WiFi.getMode()) {
-            DBG_OUT << "connecting <" << WiFi.SSID() << "> " << endl;
-            return;
-        }
+        DBG_OUT << "connecting <" << WiFi.SSID() << "> " << endl;
+    }
 }
 
 void setup_signals() {
     DBG_FUNK();
-    LDRSignal.onChange([](const uint8_t &level) {
-        matrix.setIntensity(level);
-    }
-    );
-
-    timeKeeper.onChange([](const time_t &time) {
-        if (timeNotSet == timeStatus()) {
-            return;
-        }
-        static auto preMinute = static_cast<uint8_t>(0xff);
-        const auto curMinute = minute(time);
-        if (curMinute == preMinute) {
-            return;
-        }
-        preMinute = curMinute;
-        matrix.fillScreen(LOW);
-        matrix.setCursor(0, 7);
-        char buffMin[6];
-        sprintf_P(buffMin, "%2u:%02u", hour(time), curMinute);
-        matrix.print(buffMin);
-        matrix.write();
-    });
+    LDRSignal.onChange([](const uint8_t& level) { matrix.setIntensity(level); });
 }
 
 void setup_mqtt() {
     DBG_FUNK();
     mqtt.setup(config.getCSTR("MQTT_SERVER"), config.getInt("MQTT_PORT"), pDeviceName);
-    string topic = "cmd/";
-    topic += pDeviceName;
-    mqtt.callback(topic, [](char *topic, uint8_t *payload,
-                            unsigned int length) {
-      DBG_OUT << "MQTT>>[" << topic << "]:";
-      auto tt = reinterpret_cast<const char *>(payload);
-      auto i = length;
-      while (i--) {
-        DBG_OUT << *tt;
-        tt++;
-      };
-      DBG_OUT << endl;
-      StaticJsonDocument<512> json_cmd;
-      DeserializationError error = deserializeJson(json_cmd, payload, length);
-      if (error) {
-        DBG_OUT << "Failed to read file, using default configuration" << endl;
-      } else {
-        //        if (json_cmd.containsKey("led")) {
-        //          ledCmdSignal.set(json_cmd["led"]);
-        //        }
-        //        if (json_cmd.containsKey("time")) {
-        //          ledCmdSignal.set(json_cmd["led"]);
-        //        }
-      }
+
+    static auto mqtt_el = event_loop::set_interval(
+        []() {
+            if (mqtt.isConnected()) {
+                string topic = "stat/";
+                topic += pDeviceName;
+                StaticJsonDocument<512> sensors;
+                if (!isnan(dht.getTemperature())) {
+                    sensors["temperature"] = dht.getTemperature();
+                }
+                if (!isnan(dht.getHumidity())) {
+                    sensors["humidity"] = dht.getHumidity();
+                }
+
+                sensors["ldr"]  = static_cast<unsigned>(LDRSignal.getLastValue());
+                sensors["rssi"] = WiFi.RSSI();
+                sensors["ip"]   = WiFi.localIP();
+                String json_string;
+                serializeJson(sensors, json_string);
+                mqtt.publish(topic, json_string.c_str());
+            }
+        },
+        5min, true);
+
+    mqtt.on_connection_change([](const auto is_connected) {
+        DBG_OUT << "setup_mqtt=" << is_connected << endl;
+
+        if (is_connected) {
+            mqtt_el->trigger();
+        } else {
+            event_loop::set_timeout([]() { mqtt.connect(); }, 30s);
+        }
     });
+
+    static WiFiEventHandler onStationModeGotIP = WiFi.onStationModeGotIP(
+        [](auto event) { event_loop::set_timeout([]() { mqtt.connect(); }, 100ms); }); // need to lanch froom loop
 }
 
 void setup_config() {
     config.getConfig().clear();
-    config.getConfig()["DEVICE_NAME"] = "CLOCK";
-    config.getConfig()["MQTT_SERVER"] = "";
-    config.getConfig()["MQTT_PORT"] = 0;
-    config.getConfig()["MQTT_PERIOD"] = 60 * 1000;
-    config.getConfig()["OTA_USERNAME"] = "";
-    config.getConfig()["OTA_PASSWORD"] = "";
+    config.getConfig()["DEVICE_NAME"]         = "CLOCK";
+    config.getConfig()["MQTT_SERVER"]         = "";
+    config.getConfig()["MQTT_PORT"]           = 0;
+    config.getConfig()["MQTT_PERIOD"]         = 60 * 1000;
+    config.getConfig()["OTA_USERNAME"]        = "";
+    config.getConfig()["OTA_PASSWORD"]        = "";
     config.getConfig()["LED_MATRIX_ROTATION"] = 0;
-    config.getConfig()["LED_MATRIX_I_MAX"] = 15;
-    config.getConfig()["LDR_MIN"] = 0;
-    config.getConfig()["LDR_MAX"] = 1000;
+    config.getConfig()["LED_MATRIX_I_MAX"]    = 15;
+    config.getConfig()["LDR_MIN"]             = 0;
+    config.getConfig()["LDR_MAX"]             = 1000;
+    config.getConfig()["ntpServerName"]       = "time.nist.gov";
     if (!config.load("/www/config/config.json")) {
-        //write file
+        // write file
         config.write("/www/config/config.json");
     }
     pDeviceName = config.getCSTR("DEVICE_NAME");
+}
+
+void show_time() {
+    DBG_FUNK();
+    const auto local = myTZ.toLocal(ntp_client.getEpochTime());
+    char       buffMin[6];
+    sprintf_P(buffMin, "%2u:%02u", hour(local), minute(local));
+    display_print(buffMin);
+    DBG_OUT << "utc time:" << ntp_client.getFormattedTime() << endl;
+    event_loop::set_timeout(show_time, 60s - std::chrono::seconds(ntp_client.getSeconds()));
 }
 
 void setup() {
@@ -274,10 +292,10 @@ void setup() {
 
     hw_info(DBG_OUT);
     LittleFS.begin();
+    event_loop::init();
     setup_config();
     LDRSignal.setup();
-    LDRSignal.setRange(config.getInt("LDR_MIN"), config.getInt("LDR_MAX"), 0,
-                       config.getInt("LED_MATRIX_I_MAX"));
+    LDRSignal.setRange(config.getInt("LDR_MIN"), config.getInt("LDR_MAX"), 0, config.getInt("LED_MATRIX_I_MAX"));
     MDNS.addService("http", "tcp", SERVER_PORT_WEB);
     MDNS.begin(pDeviceName);
     setup_WebPages();
@@ -286,53 +304,25 @@ void setup() {
     LittleFS_info(DBG_OUT);
     setup_matrix();
     setup_mqtt();
-
-    ntpTime.init();
+    ntp_client.setPoolServerName(config.getCSTR("ntpServerName"));
+    ntp_client.begin();
     //------------------
     dht.setup(DHTPin, DHTesp::DHT22);
     //-----------------
-    matrix.fillScreen(LOW);
-    matrix.setCursor(0, 7);
-    matrix.print(pDeviceName);
-    matrix.write();
+    display_print(pDeviceName);
     setup_WIFIConnect();
     DBG_OUT << "Setup done" << endl;
 }
 
-static unsigned long nextMsgMQTT = 0;
-
-void mqtt_send() {
-    if (mqtt.isConnected()) {
-        nextMsgMQTT = millis() + config.getULong("MQTT_PERIOD");
-        string topic = "stat/";
-        topic += pDeviceName;
-        ostringstream payload;
-        get_status(payload);
-        DBG_OUT << "MQTT<<[" << topic << "]:" << payload.str() << endl;
-        mqtt.publish(topic, payload.str());
-    } else {
-        nextMsgMQTT = 0; //force to send after connection
-    }
-}
-
-void mqtt_loop() {
-    if (WL_CONNECTED != WiFi.status()) {
-        return;
-    }
-    mqtt.loop();
-
-    if (millis() >= nextMsgMQTT) { //send
-        mqtt_send();
-    }
-
-}
-
 void loop() {
-    wifiStateSignal.loop();
-    mqtt_loop();
-    ntpTime.loop();
     LDRSignal.loop();
-    timeKeeper.loop();
     serverWeb.handleClient();
-    event_loop.loop();
+    event_loop::loop();
+    mqtt.loop();
+    ntp_client.update();
+    static bool wait_for_first_ntp = true;
+    if (wait_for_first_ntp && ntp_client.isTimeSet()) {
+        wait_for_first_ntp = false;
+        show_time();
+    }
 }
